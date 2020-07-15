134da54f3d9ce4e726e3d0f6173d9d2063455598
George Rashkov <georg.rashkov@gmail.com>
2020-07-15 18:12:32 +0300

    prime

--- bot_logic.py
+++ bot_logic.py
@@ -3,13 +3,21 @@ import json
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
 
+options = Options()
+ua = UserAgent()
 driver = webdriver.ChromeOptions()
-# driver = webdriver.Chrome("chromedriver.exe")
 prefs = {"profile.managed_default_content_settings.images": 2}
+userAgent = ua.random
+print(userAgent)
+options.add_argument(f'user-agent={userAgent}')
+driver = webdriver.Chrome( executable_path='D:/develop/pypr/bot_messages_template/chromedriver',options=driver)
+
+
 # driver.add_experimental_option("prefs", prefs)
 # driver.add_argument("headless")
-driver = webdriver.Chrome(executable_path='D:/develop/pypr/bot_messages_template/chromedriver', options=driver)
 
 def get_profiles():
     with open('profiles.txt') as f:
@@ -29,22 +37,26 @@ def reply(id_profile, id_chat, message):
 def notify(id_profile):
     pass
 
+
 def login(username, password):
     id_profile = int()
     return id_profile
 
 
 def test():
-    driver.get('https://prime.date/auth')
+    accounts = [
+        ['zhelyaskova1M@gmail.com', '54321012'],
+        ['2107miker@gmail.com', '2107miker'],
+    ]
+    for i in range(2):
+        for j in range(2):
+            driver.get('https://prime.date/auth')
+            time.sleep(3)
+            driver.find_element_by_xpath("/html/body/div[1]/div[4]/div/form/div[2]/input").send_keys(accounts[i][0])
+            driver.find_element_by_xpath("/html/body/div[1]/div[4]/div/form/div[3]/input").send_keys(accounts[i][1])
+            driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/form/button').click()
 
-    driver.find_element_by_xpath("/html/body/div[1]/div[4]/div/form/div[2]/input").send_keys('2107miker@gmail.com')
-    driver.find_element_by_xpath("/html/body/div[1]/div[4]/div/form/div[3]/input").send_keys('2107miker')
+        driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
 
-    driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/form/button').click()
-    # driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
-    # driver.get('https://www.ukr.net')
-
-    time.sleep(5)
-
-    driver.close()
+    # driver.close()
 test()
